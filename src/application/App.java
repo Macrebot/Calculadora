bb625e07b135b7e144a71b64134d8f007ce5d9c1
Macrebot <luis.maldo@protonmail.com>
2022-11-07 22:21:17 +0100

    Commit de inicio de projecto Interfaz gráfica

---
+++ src/application/App.java
@@ -0,0 +1,24 @@
+package application;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class App extends Application{
+
+    @Override
+    public void start(Stage primaryStage){
+        StackPane root = new StackPane();
+
+        Scene scene = new Scene(root, 400, 400);
+
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Titulo Interfaz");
+        primaryStage.show();
+
+    }
+    public static void main(String[] args) throws Exception {
+        launch(args);
+    }
+}
