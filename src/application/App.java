b16e774fc13ff4c1bc003805b6764273a6280980
Macrebot <luis.maldo@protonmail.com>
2022-11-11 20:26:18 +0100

    Ultimo commit del dia

--- src/application/App.java
+++ src/application/App.java
@@ -4,115 +4,111 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 
+import java.util.concurrent.CountDownLatch;
+
+import application.functions.Functions;
+
 public class App extends Application{
-
-    public static int Suma(int a, int b){
-        return a + b;
-    }
-
-    public static int Resta(int a, int b){
-        return a - b;
-    }
-
-    public static int Multiplicacion(int a, int b){
-        return a * b;
-    }
-
-    public static double Division (int a, int b){
-        return (double)a / (double)b;
-    }
 
     @Override
     public void start(Stage primaryStage){
+        Functions F = new Functions();
         GridPane root = new GridPane();
 
         Label labelRespuesta = new Label("No hay resultado aún.");
-        Label label1 = new Label("Introduce número 1: ");
-        Label label2 = new Label("Introduce número 2: ");
-        Label lEmpty = new Label();
-        Label error = new Label("No hay errores.");
 
-        TextField fieldNum1 = new TextField();
-        TextField fieldNum2 = new TextField();
+        Button btnSuma = new Button("+");
+        Button btnResta = new Button("-");
+        Button btnMultiplicacion = new Button("*");
+        Button btnDivision = new Button("/");
+        Button btnResultado = new Button("=");
 
-        Button btnSuma = new Button("Sumar");
-        Button btnResta = new Button("Restar");
-        Button btnMultiplicacion = new Button("Multiplicacion");
-        Button btnDivision = new Button("División");
+        Button btnCero = new Button("0");
+        Button btnUno = new Button("1");
+        Button btnDos = new Button("2");
+        Button btnTres = new Button("3");
+        Button btnCuatro = new Button("4");
+        Button btnCinco = new Button("5");
+        Button btnSeis = new Button("6");
+        Button btnSiete = new Button("7");
+        Button btnOcho = new Button("8");
+        Button btnNueve = new Button("9");
 
         btnSuma.setOnAction(new EventHandler<ActionEvent>(){
 
             @Override
             public void handle(ActionEvent e){
-                String stringRespuesta;
-
-                int intRespuesta = Suma(
-                    Integer.parseInt(fieldNum1.getText()), 
-                    Integer.parseInt(fieldNum2.getText()));
-
-                System.out.println(intRespuesta);
-                stringRespuesta = Integer.toString(intRespuesta);
-                labelRespuesta.setText("El resultado es " + stringRespuesta);
             }
         });
 
         btnResta.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent e){
-                String stringRespuesta;
-
-                int intRespuesta = Resta(
-                    Integer.parseInt(fieldNum1.getText()),
-                    Integer.parseInt(fieldNum2.getText()));
-
-                stringRespuesta = Integer.toString(intRespuesta);
-                labelRespuesta.setText("El resultado es: " + stringRespuesta);
             }
         });
 
         btnMultiplicacion.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent e){
-                String stringRespuesta;
-
-                int intRespuesta = Multiplicacion(
-                    Integer.parseInt(fieldNum1.getText()), 
-                    Integer.parseInt(fieldNum2.getText()));
-
-                stringRespuesta = Integer.toString(intRespuesta);
-                labelRespuesta.setText("El resultado es: " + stringRespuesta);
             }
         });
 
         btnDivision.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent e){
-                String stringRespuesta;
-
-                double doubleRespuesta = Division(
-                    Integer.parseInt(fieldNum1.getText()), 
-                    Integer.parseInt(fieldNum2.getText()));
-
-                stringRespuesta = Double.toString(doubleRespuesta);
-                labelRespuesta.setText("El resultado es: " + stringRespuesta);
             }
         });
 
-        root.addRow(0, label1, fieldNum1);
-        root.addRow(1, label2, fieldNum2);
-        root.addRow(2, lEmpty);
-        root.addRow(3, labelRespuesta);
-        root.addRow(5, btnSuma);
-        root.addRow(6, btnResta);
-        root.addRow(7, btnMultiplicacion);
-        root.addRow(8, btnDivision);
-        root.addRow(9, error);
+        root.add(labelRespuesta, 0, 0, 3, 2);
+        labelRespuesta.setMaxSize(Double.MAX_VALUE, 100);
+        labelRespuesta.setMinSize(Double.MAX_VALUE, 100);
+
+        int btnSizes = 100;
+
+        root.add(btnNueve,  2, 3);
+        btnNueve.setMinSize(btnSizes, btnSizes);
+        btnNueve.setMaxSize(btnSizes, btnSizes);
+
+        root.add(btnOcho,   1, 3);
+        btnOcho.setMinSize(btnSizes, btnSizes);
+        btnOcho.setMaxSize(btnSizes, btnSizes);
+
+        root.add(btnSiete,  0, 3);
+        btnSiete.setMinSize(btnSizes, btnSizes);
+        btnSiete.setMaxSize(btnSizes, btnSizes);
+
+        root.add(btnSeis,   2, 4);
+        btnSeis.setMinSize(btnSizes, btnSizes);
+        btnSeis.setMaxSize(btnSizes, btnSizes);
+
+        root.add(btnCinco,  1, 4);
+        btnCinco.setMinSize(btnSizes, btnSizes);
+        btnCinco.setMaxSize(btnSizes, btnSizes);
+
+        root.add(btnCuatro, 0, 4);
+        btnCuatro.setMinSize(btnSizes, btnSizes);
+        btnCuatro.setMaxSize(btnSizes, btnSizes);
+
+        root.add(btnTres,   2, 5);
+        btnTres.setMinSize(btnSizes, btnSizes);
+        btnTres.setMaxSize(btnSizes, btnSizes);
+
+        root.add(btnDos,    1, 5);
+        btnDos.setMinSize(btnSizes, btnSizes);
+        btnDos.setMaxSize(btnSizes, btnSizes);
+
+        root.add(btnUno,    0, 5);
+        btnUno.setMinSize(btnSizes, btnSizes);
+        btnUno.setMaxSize(btnSizes, btnSizes);
+
+        root.add(btnCero,   1, 6);
+        btnCero.setMinSize(btnSizes, btnSizes);
+        btnCero.setMaxSize(btnSizes, btnSizes);
 
         Scene scene = new Scene(root, 500, 500);
 
@@ -125,5 +121,4 @@ public class App extends Application{
         launch(args);
     }
 
-	/* Comentario prueba */
 }
--- src/application/functions/Functions.java
+++ src/application/functions/Functions.java
@@ -1,19 +1,19 @@
 package application.functions;
 
 public class Functions {
-    public static int Suma(int a, int b){
+    public int Suma(int a, int b){
         return a + b;
     }
 
-    public static int Resta(int a, int b){
+    public int Resta(int a, int b){
         return a - b;
     }
 
-    public static int Multiplicacion(int a, int b){
+    public int Multiplicacion(int a, int b){
         return a * b;
     }
 
-    public static double Division (int a, int b){
+    public double Division (int a, int b){
         return (double)a / (double)b;
     }
 }
